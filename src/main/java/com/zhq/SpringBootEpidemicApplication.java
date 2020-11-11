10eadca6c658cafbc350aea9599a2a09b26ec001
zhqailm <1398326910@qq.com>
2020-11-11 14:56:15 +0800

    day 1

---
+++ src/main/java/com/zhq/SpringBootEpidemicApplication.java
@@ -0,0 +1,13 @@
+package com.zhq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEpidemicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEpidemicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhq/SpringBootEpidemicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEpidemicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
